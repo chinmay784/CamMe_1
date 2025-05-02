200c3b9b8870f8cb727137ffb02b59e4781a297d
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-05-02 09:48:59 +0530

    hel

--- swagger.js
+++ swagger.js
@@ -21,7 +21,7 @@ const swaggerDefinition = {
    
     servers: [
         {
-            url: 'https://camme-1-1.onrender.com',
+            url: 'https://camme-1-1.onrender.com/api/v1',
         },
     ],
 }
