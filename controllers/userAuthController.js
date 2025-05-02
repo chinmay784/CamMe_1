0b1ce2f25e32d12ac6144447e09bde574ae528d2
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-05-02 19:10:15 +0530

    edit

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -55,11 +55,15 @@ exports.register = async (req, res) => {
         const otpExpires = Date.now() + 10 * 60 * 200;
 
         // const {path} = req.file
+        const file = req.file?.path;
+        const result = await cloudinary.uploader.upload(file, {
+            folder: "profile_pics", // more appropriate than "profile_pics"
+        });
 
         user = new User({
             gender,
             theme,
-            profilePic: file ? result.secure_url : `https://api.dicebear.com/5.x/initials/svg?seed=${encodeURIComponent(fullName)}`,
+            profilePic: result ? result.secure_url : `https://api.dicebear.com/5.x/initials/svg?seed=${encodeURIComponent(fullName)}`,
             fullName,
             dateBirth,
             email,
