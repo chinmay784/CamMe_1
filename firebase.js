255f2b663805b3fd4653f47f8637104f92bf50da
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-05-26 11:55:25 +0530

    edit

--- firebase.js
+++ firebase.js
@@ -1,8 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const admin = require("firebase-admin");
 const serviceAccount = require("./console.json"); // Download from Firebase Console
+const {applicationDefault} = require("firebase-admin/app")
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: applicationDefault(),
+  projectId:"potion-for-creators"
 });
 
 module.exports = admin;
