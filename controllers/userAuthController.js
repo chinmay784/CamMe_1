ef88547a052797027139dcaa353520167413d560
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-06-01 09:38:17 +0530

    edit

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -484,6 +484,7 @@ exports.loginOtpverify = async (req, res) => {
         user.otpExpires = undefined;
         user.fcmToken = fcmToken;
         await user.save()
+        console.log(user.fcmToken)
 
         return res.status(200).json({
             sucess: true,
